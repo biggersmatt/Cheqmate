37de2015133b6ae2cd358c6d01d77235783d729d
biggersmatt <biggersmatt73@gmail.com>
2021-01-16 17:00:49 -0800

    Created post route for new ticket

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,7 +9,15 @@ router.get('/', (req, res) => {
 
 // GET new ticket
 router.get('/new', (req, res) => {
-  res.render('./user/new');
+  res.render('./user/ticketnew');
+})
+
+// POST new ticket to database
+router.post('/', (req, res) => {
+  // db.Ticket.create(req.body, (err, newTicket) => {
+  //     if(err) console.log(err);
+  //   })
+    res.redirect('./user/dashboard');
 })
 
 module.exports = router;
