7f72b020ec4062766f2582789465d7887bd7406d
biggersmatt <biggersmatt73@gmail.com>
2021-04-28 14:11:16 -0700

    Modifies styles on Dashboard Admin bar

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,38 +1,38 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../database');
+const db = require("../database");
 
-// Current route '/user'
+// Current route "/user"
 
 // Render Sign Up Page
-router.get('/new', (req, res) => {
-  res.render('./user/userNew');
+router.get("/new", (req, res) => {
+  res.render("./user/userNew");
 })
 
 /****POST****/
 // Create New User
 // Redirect to Login Page
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   db.User.create(req.body, (err, userCreated) => {
     if(err) console.log(err);
-    res.redirect('/');
+    res.redirect("/");
   });
 });
 
 /****POST****/
 // Submit Login Info
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   db.User.findOne({username: req.body.username}, (err, foundUser) => {
     if(err) console.log(err);
-    if(!foundUser) res.redirect('/');
+    if(!foundUser) res.redirect("/");
     if(foundUser.password === req.body.password) res.redirect(`/user/${foundUser.id}`);
-    res.render('index');
+    res.render("index");
   });
 });
 
 /****GET****/
 // Populate Infomation for Dashboard
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   db.User.findById(req.params.id, (err, foundUser) => {
     if(err) console.log(err);
     db.Ticket.find({user: req.params.id}, (err, userTickets) => {
@@ -40,9 +40,8 @@ router.get('/:id', (req, res) => {
       const context = {
         user: foundUser,
         tickets: userTickets,
-        
       }
-      res.render('./user/userShow', context);
+      res.render("./user/userShow", context);
     })
   });
 });
